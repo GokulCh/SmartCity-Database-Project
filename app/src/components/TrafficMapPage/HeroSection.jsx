5c5d325019da4d410f37e133acc034b28b84801f
GokulCh <gokul.chaluvadi@gmail.com>
2024-12-09 16:59:55 -0500

    Update Traffic Map references in NavigationBar and HeroSection for clarity and improved user experience

--- app/src/components/TrafficMapPage/HeroSection.jsx
+++ app/src/components/TrafficMapPage/HeroSection.jsx
@@ -5,11 +5,11 @@ const HeroSection = () => {
     <section id="Home" className="w-full flex flex-col items-center justify-center py-24 sm:py-36 h-screen relative">
       <div id="HeroSectionContainer" className="flex flex-col items-center justify-center text-center space-y-6">
         <h1 className="text-4xl sm:text-5xl font-bold fade-in-medium">
-          <span className="text-blue-500">SmartCity</span> <span className="text-white">Traffic Hub + Queries</span>
+          <span className="text-blue-500">SmartCity</span> <span className="text-white">Traffic Map</span>
         </h1>
         <p className="text-base max-w-3xl mx-auto">
-          Explore our comprehensive queries to gain insights into traffic patterns and accident data. Empowering
-          Richmond's residents with real-time safety information and proactive navigation solutions.
+          View real-time traffic conditions and accident reports. Empowering Richmond's residents with up-to-date
+          traffic information and safer navigation routes.
         </p>
       </div>
 
